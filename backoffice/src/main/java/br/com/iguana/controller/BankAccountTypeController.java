ace9a8d767049785ecd4aee5aa4e7a95ad7ccd68
hugo iguana <hugo.iguanaa@gmail.com>
2021-01-22 17:53:14 -0300

    Small adjusts.

--- backoffice/src/main/java/br/com/iguana/controller/BankAccountTypeController.java
+++ backoffice/src/main/java/br/com/iguana/controller/BankAccountTypeController.java
@@ -3,23 +3,16 @@ package br.com.iguana.controller;
 import br.com.iguana.dtos.BankAccountTypeDto;
 import br.com.iguana.entities.BankAccountType;
 import br.com.iguana.services.IBankAccountTypeService;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 import java.util.List;
 import java.util.Optional;
-
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class BankAccountTypeController implements IBankAccountTypeController {
 
@@ -36,7 +29,7 @@ public class BankAccountTypeController implements IBankAccountTypeController {
     }
 
     @Override
-    public Response getById(@Parameter(description = "Bank Accounts Type identifier", required = true) @PathParam("id") Long id) {
+    public Response getById(Long id) {
         Optional<BankAccountType> bankAccount = service.findById(id);
         if (bankAccount.isPresent()) {
             LOGGER.debug("Found Bank Account Type " + bankAccount.toString());
@@ -49,9 +42,7 @@ public class BankAccountTypeController implements IBankAccountTypeController {
     }
 
     @Override
-    public Response create(
-            @RequestBody(required = true, content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = BankAccountTypeDto.class))) @Valid BankAccountTypeDto dto
-            , @Context UriInfo uriInfo) {
+    public Response create(@Valid BankAccountTypeDto dto, @Context UriInfo uriInfo) {
 
         LOGGER.debug("Receiving request to create a new Bank Account Type.");
         BankAccountType BankAccountType = dto.buildEntity();
@@ -65,7 +56,7 @@ public class BankAccountTypeController implements IBankAccountTypeController {
     }
 
     @Override
-    public Response update(@RequestBody(required = true, content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = BankAccountTypeDto.class))) @Valid BankAccountTypeDto dto) {
+    public Response update(@Valid BankAccountTypeDto dto) {
         BankAccountType BankAccountType = dto.buildEntity();
         BankAccountType = service.update(BankAccountType);
         LOGGER.debug("Bank Account Type updated with new valued " + BankAccountType.toString());
@@ -73,7 +64,7 @@ public class BankAccountTypeController implements IBankAccountTypeController {
     }
 
     @Override
-    public Response delete(@Parameter(description = "Bank Account Type identifier", required = true) @PathParam("id") Long id) {
+    public Response delete(Long id) {
         service.delete(id);
         LOGGER.debug("Bank Account Type deleted with id = " + id);
         return Response.noContent().build();
